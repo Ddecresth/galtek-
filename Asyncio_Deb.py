46845a6eee7a1d92627dca1db9319fa021f9dcb2
Ddecresth <54872083+Ddecresth@users.noreply.github.com>
2019-09-11 01:09:08 -0400

    Add files via upload

---
+++ Asyncio_Deb.py
@@ -0,0 +1,23 @@
+import asyncio
+
+Class ClientProtocol(asyncio.Protocol):
+	Def __init__(self,loop):
+		Self.loop=loop
+	Def connection_made(self,transport):
+		Self.transport=transport
+	Def data_received(self.data):
+		Print('Data received: {!r}'.format(data.decode()))
+		Command=input()
+		Command=command.encode() + b"<EOL>\n"
+		Self.transport.write(command)
+	Def connection_lost(self,exc):
+		Print('connection lost!')
+		Self.loop.stop()
+		
+HOST="0.0.0.0"
+PORT="1234"
+Loop=asyncio.get_event_loop()
+Coro=loop.create_connection(lambda:ClientProtocol(loop),
+Loop.run_until_complete(coro)
+Loop.run_forever()
+Exit()
